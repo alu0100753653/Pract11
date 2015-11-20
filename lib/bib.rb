137509483dd9ad20129866b30707a0f9ce5d55d3
alu0100753653 <alu0100753653@ull.edu.es>
2015-11-20 07:28:39 +0000

    Creadas Clases Hijas: Libro,Revista,Electronico

--- lib/bib.rb
+++ lib/bib.rb
@@ -18,4 +18,33 @@ module Bib
         "(#{autores},#{titulo},#{serie},#{editorial},#{edicion},#{fecha})"
         end
     end
+    
+    class Libro < Biblio
+    
+        attr_accessor :isbn
+        
+        def initialize(autor, titulo, serie, editorial, edicion, fecha, isbn)
+        super(autor, titulo, serie, editorial, edicion, fecha)
+            @isbn = isbn
+        end
+    end
+
+    class Revista < Biblio
+            
+    	attr_accessor :revista
+            
+    	def initialize(autor, titulo, serie, editorial, edicion, fecha, revista)
+    		super(autor, titulo, serie, editorial, edicion, fecha)
+    			@revista = revista
+        end
+    end
+
+    class Electronico < Biblio
+    	attr_accessor :url
+            
+    	def initialize(autor, titulo, serie, editorial, edicion, fecha, url)
+    		super(autor, titulo, serie, editorial, edicion, fecha)
+            @url = url
+        end
+    end
 end
