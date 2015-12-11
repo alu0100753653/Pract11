3f2b2ac028865abc872d50efc47e1862e6d820ac
alu0100753653 <alu0100753653@ull.edu.es>
2015-12-11 14:41:27 +0000

    Coveralls3

--- spec/module_spec.rb
+++ spec/module_spec.rb
@@ -9,8 +9,23 @@ include Bib
 describe Bib do
     
     before :all do
+        @refer1=Libro.new(['Juan Perez'],'El amor es para tontos','Santillana',1,2,2003)
+        @refer2=Libro.new(['Pedro ALvarez'],'Mienteme','Planeta',5,1,2015)
+        @refer3=Periodico.new(['Ana Suarez'], 'Elecciones',2015,'El pais',4)
+        @refer4=Periodico.new(['Luis Gonzalez'], 'Partidos Inutiles',2015,'El Dia',2)
+        @refer5=Electronico.new(['Martina Espinosa'],'A Beautiful Mind',2001,1,'Londres','www.writter.com')
+        
+        @listarefer=ReferList.new()
         
         
     end
+    
+    it "Existe una lista de referencias" do
+        expect(@listarefer).to_not be nil
+    end
+    
+    
+    
+    
     
 end
